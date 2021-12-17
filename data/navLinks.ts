84c695a1ddea22e2f268a62438747a80039d543a
MiryangJung <miryang.dev@gmail.com>
2021-12-18 07:27:22 +0900

    Edit note navlink

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -2,7 +2,7 @@ const navLinks: { title: string; link: string }[] = [
   { title: 'Home', link: '/' },
   { title: 'Blog', link: '/blog' },
   { title: 'Projects', link: '/projects' },
-  { title: 'Note', link: '/note' },
+  { title: 'Note', link: '/note/golang/Convertrawtopng' },
   { title: 'About', link: '/about' },
 ]
 
--- pages/note.tsx
+++
@@ -1,5 +0,0 @@
-function Notepage() {
-  return <>Note page NOT YET</>
-}
-
-export default Notepage
