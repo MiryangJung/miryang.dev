265f862dfa6656e79af2f3fab8603637180cd362
MiryangJung <miryang.dev@gmail.com>
2022-01-11 17:31:30 +0900

    Edit note link

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -2,7 +2,7 @@ const navLinks: { title: string; link: string }[] = [
   { title: 'Home', link: '/' },
   { title: 'Blog', link: '/blog' },
   { title: 'Projects', link: '/projects' },
-  { title: 'Note', link: '/note/golang/Convertrawtopng' },
+  { title: 'Note', link: '/note/nextjs/note-frontendmasters-nextjs-introduction' },
   { title: 'About', link: '/about' },
   { title: 'Resume', link: '/resume' },
 ]
