bd7e72819dc185903ed7f9609bc98641bc313a10
MiryangJung <miryang.dev@gmail.com>
2022-01-21 21:27:35 +0900

    Edit note meta

--- layouts/note.tsx
+++ layouts/note.tsx
@@ -3,11 +3,15 @@ import NoteAside from '../components/NoteAside'
 import MDXPost from '../components/MDXPost'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 import Container from '../components/Container'
+import metadata from '../data/metadata'
 
 function NoteLayout({ tree, note }) {
   const customMeta = {
     title: note.title,
     description: 'Code Snippets or Code note',
+    image: `${metadata.meta.url}/note/note.png`,
+    type: 'article',
+    date: new Date(note.date).toISOString(),
   }
   const MDXComponent = useMDXComponent(note.body.code)
   return (
