995f34b134720cdc3c964d79613085e0333c9d4e
MiryangJung <miryang.dev@gmail.com>
2022-02-22 21:52:59 +0900

    Post Docs. Let Binding

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -1,7 +1,7 @@
 const navLinks: { title: string; link: string }[] = [
   { title: 'Blog', link: '/blog' },
   { title: 'Projects', link: '/projects' },
-  { title: 'Note', link: '/note/nextjs/note-frontendmasters-nextjs-introduction' },
+  { title: 'Note', link: '/http://localhost:3000/note/rescript/docs-lang-feat-let-binding' },
   { title: 'About', link: '/about' },
   { title: 'Resume', link: '/resume' },
   { title: 'GuestBook', link: '/guestbook' },
