0613c08886b6a81db74d2767902f1e3dd371dcfa
MiryangJung <miryang.dev@gmail.com>
2022-06-16 19:38:51 +0900

    pageExtensions 에 bs.js 추가

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { withContentlayer } = require('next-contentlayer')
 
 module.exports = withContentlayer({
   reactStrictMode: true,
+  pageExtensions: ['jsx', 'js', 'ts', 'tsx', 'bs.js'],
   eslint: {
     dirs: ['pages', 'components', 'lib', 'layouts', 'data'],
   },
