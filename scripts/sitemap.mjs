2d0ebb08af66641b2b85bbc996719226f6fd96e4
MiryangJung <miryang.dev@gmail.com>
2022-06-16 19:20:23 +0900

    sitemap prettier

--- scripts/sitemap.mjs
+++ scripts/sitemap.mjs
@@ -1,47 +1,47 @@
-import {writeFileSync} from 'fs';
-import {globby} from 'globby';
-import prettier from 'prettier';
+import { writeFileSync } from 'fs'
+import { globby } from 'globby'
+import prettier from 'prettier'
 
 async function generate() {
-    const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
-    const pages = await globby([
-        'pages/*.js',
-        'pages/*.tsx',
-        'posts/**/*.mdx',
-        '!pages/_*.tsx',
-        '!pages/_*.js',
-    ]);
+  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
+  const pages = await globby([
+    'pages/*.js',
+    'pages/*.tsx',
+    'posts/**/*.mdx',
+    '!pages/_*.tsx',
+    '!pages/_*.js',
+  ])
 
-    const sitemap = `
+  const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${pages
-        .map((page) => {
+          .map(page => {
             const path = page
-                .replace('pages', '')
-                .replace('posts', '')
-                .replace('.tsx', '')
-                .replace('.js', '')
-                .replace('.mdx', '')
-                .replace('/index','');
-            
+              .replace('pages', '')
+              .replace('posts', '')
+              .replace('.tsx', '')
+              .replace('.js', '')
+              .replace('.mdx', '')
+              .replace('/index', '')
+
             return `
               <url>
                   <loc>${`https://miryang.dev${path}`}</loc>
               </url>
-            `;
-        })
-        .join('')}
+            `
+          })
+          .join('')}
     </urlset>
-    `;
+    `
 
-    const formatted = prettier.format(sitemap, {
-        ...prettierConfig,
-        parser: 'html'
-    });
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: 'html',
+  })
 
-    // eslint-disable-next-line no-sync
-    writeFileSync('public/sitemap.xml', formatted);
+  // eslint-disable-next-line no-sync
+  writeFileSync('public/sitemap.xml', formatted)
 }
 
-generate();
+generate()
