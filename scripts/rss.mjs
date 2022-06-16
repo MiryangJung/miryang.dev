f0099f7f70760315ebb9e44e673a0df93b5e3eff
MiryangJung <miryang.dev@gmail.com>
2022-06-16 19:19:48 +0900

    rss 생성 위치 변경

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -1,34 +1,34 @@
-import {writeFileSync} from 'fs';
-import RSS from 'rss';
+import { writeFileSync } from 'fs'
+import RSS from 'rss'
 
-import {allBlogs} from '../.contentlayer/generated/allBlogs.mjs';
-import {allNotes} from '../.contentlayer/generated/allNotes.mjs';
+import { allBlogs } from '../.contentlayer/generated/Blog/_index.mjs'
+import { allNotes } from '../.contentlayer/generated/Note/_index.mjs'
 
 async function generate() {
-    const feed = new RSS({
-        title: 'MiryangJung',
-        site_url: 'https://miryang.dev',
-        feed_url: 'https://miryang.dev/feed.xml'
-    });
+  const feed = new RSS({
+    title: 'MiryangJung',
+    site_url: 'https://miryang.dev',
+    feed_url: 'https://miryang.dev/feed.xml',
+  })
 
-    allBlogs.map((post) => {
-        feed.item({
-            title: post.title,
-            url: `https://miryang.dev/blog/${post.slug}`,
-            date: post.date,
-            description: post.description
-        });
-    });
+  allBlogs.map(post => {
+    feed.item({
+      title: post.title,
+      url: `https://miryang.dev/blog/${post.slug}`,
+      date: post.date,
+      description: post.description,
+    })
+  })
 
-    allNotes.map((note) => {
-        feed.item({
-            title: note.title,
-            url: `https://miryang.dev/${note.url_path}`,
-            date: note.date,
-        });
-    });
+  allNotes.map(note => {
+    feed.item({
+      title: note.title,
+      url: `https://miryang.dev/${note.url_path}`,
+      date: note.date,
+    })
+  })
 
-    writeFileSync('./public/feed.xml', feed.xml({ indent: true }));
+  writeFileSync('./public/feed.xml', feed.xml({ indent: true }))
 }
 
-generate();
+generate()
