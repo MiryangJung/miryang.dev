4998e1730012cb73ce97fcd8a872307ea3e075e4
MiryangJung <miryang.dev@gmail.com>
2022-07-19 01:35:22 +0900

    update project card style

--- components/ProjectCards.style.ts
+++ components/ProjectCards.style.ts
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 
 export const ProjectCardsContainer = styled.div`
   width: 100%;
@@ -7,7 +7,7 @@ export const ProjectCardsContainer = styled.div`
   justify-content: space-around;
   align-items: flex-start;
   margin-top: 20px;
-`
+`;
 
 export const ProjectCard = styled.div`
   width: 300px;
@@ -22,11 +22,11 @@ export const ProjectCard = styled.div`
   .round-image {
     border-radius: 5px;
   }
-`
+`;
 
 export const ProjectCardContent = styled.div`
   position: relative;
-  top: -204px;
+  top: -206px;
   width: 300px;
   height: 200px;
   padding: 20px;
@@ -35,17 +35,19 @@ export const ProjectCardContent = styled.div`
   flex-flow: column nowrap;
   transition: 0.5s;
   opacity: 0;
-`
+`;
 
 export const ProjectCardTitle = styled.span`
   font-weight: 500;
   font-size: 24px;
   word-break: keep-all;
-`
+  color: white;
+`;
 
 export const ProjectCardDes = styled.span`
   font-weight: 200;
   font-size: 16px;
   word-break: keep-all;
   margin-top: 20px;
-`
+  color: white;
+`;
