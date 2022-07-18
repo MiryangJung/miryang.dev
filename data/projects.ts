4cf81ac6cfec0097acd0a8dac1d47ec0809a7126
MiryangJung <miryang.dev@gmail.com>
2022-07-19 01:36:19 +0900

    change projects order

--- data/projects.ts
+++ data/projects.ts
@@ -2,11 +2,11 @@ import { Project } from '../lib/types';
 
 const projects: Project[] = [
   {
-    title: 'miryang.dev',
+    title: '소주콘 Shot 1 : 진로 빨간 뚜껑',
     description:
-      'My personal website written with Next.js, MDX, Emotion, Vercel',
-    url: 'https://github.com/MiryangJung/miryang.dev',
-    image: '/projects/miryangdev.gif'
+      '소주콘 1회에서 "좋은 질문은 좋은 대답을 만든다. with relay"라는 주제로 발표',
+    url: 'https://docs.google.com/presentation/d/1OQmPNj3v_w3iMXYCir6gAvZIAvFjm9zI8Z2BTGY7q54/edit?usp=sharing',
+    image: '/projects/sojucon-1st.png'
   },
 
   {
@@ -18,11 +18,11 @@ const projects: Project[] = [
   },
 
   {
-    title: '소주콘 Shot 1 : 진로 빨간 뚜껑',
+    title: 'miryang.dev',
     description:
-      '소주콘 1회에서 "좋은 질문은 좋은 대답을 만든다. with relay"라는 주제로 발표',
-    url: 'https://docs.google.com/presentation/d/1OQmPNj3v_w3iMXYCir6gAvZIAvFjm9zI8Z2BTGY7q54/edit?usp=sharing',
-    image: '/projects/sojucon-1st.png'
+      'My personal website written with Next.js, MDX, Emotion, Vercel',
+    url: 'https://github.com/MiryangJung/miryang.dev',
+    image: '/projects/miryangdev.gif'
   }
 ];
 
