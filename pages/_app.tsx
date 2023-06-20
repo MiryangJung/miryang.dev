383d18be147a2b9d00d18c3894a89c2bf566e277
MiryangJung <miryang.dev@gmail.com>
2023-06-21 02:07:06 +0900

    vecel analytics 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
-
+import { Analytics } from '@vercel/analytics/react';
 import SEO from '../next-seo.config'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
