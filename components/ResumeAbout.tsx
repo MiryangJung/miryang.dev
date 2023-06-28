fc391cdd92d9a500a0e21fb051fba7e7a361986f
MiryangJung <miryang.dev@gmail.com>
2023-06-28 10:52:16 +0900

    lint

--- components/ResumeAbout.tsx
+++ components/ResumeAbout.tsx
@@ -1,13 +1,31 @@
-import { About, Contact, Job, Name, ResumeAboutContainer } from './ResumeAbout.style'
-import resume from '../data/resume'
-import React from 'react'
+import {
+  About,
+  Contact,
+  Job,
+  Name,
+  ResumeAboutContainer
+} from './ResumeAbout.style';
+import resume from '../data/resume';
+import React from 'react';
 
 const ResumeAbout = () => {
   return (
     <ResumeAboutContainer>
-      <Name>{resume.name}</Name>
+      <div className="flex flex-row items-center justify-between">
+        <Name>{resume.name}</Name>
+        <a
+          href={
+            'https://drive.google.com/file/d/1LMP7m52TlDOHlkODOts5wPs7-penzZsL/view?usp=sharing'
+          }
+          target={`_blank`}
+          className={`font-medium mt-1 underline`}
+        >
+          {`PDF 이력서 보기`}
+        </a>
+      </div>
       <Job>{resume.job}</Job>
       <Contact>{resume.contact}</Contact>
+
       <About>
         {resume.about.map((paragraph, index) => (
           <React.Fragment key={index}>
@@ -16,7 +34,7 @@ const ResumeAbout = () => {
         ))}
       </About>
     </ResumeAboutContainer>
-  )
-}
+  );
+};
 
-export default ResumeAbout
+export default ResumeAbout;
