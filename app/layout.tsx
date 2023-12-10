cc1b2374299551fb6aea9c577915619c46cd6e1f
MiryangJung <miryang.dev@gmail.com>
2023-12-10 20:53:59 +0900

    suite 폰트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './style/globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata } from "next";
+import "./style/globals.css";
+import { suite } from "./style/fonts/suite";
 
 export const metadata: Metadata = {
-  title: 'Miryang | Frontend Engineer',
-  description: 'miryang.dev, 프론트엔드 개발자의 블로그, Lazy Frontend Engineer who likes to travel',
-}
+  title: "Miryang | Frontend Engineer",
+  description:
+    "miryang.dev, 프론트엔드 개발자의 블로그, Lazy Frontend Engineer who likes to travel",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={suite.className}>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">sfsdfs</main>
   );
 }
