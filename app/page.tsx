2270c51f1dfe533532ad4fb9a40c8b150b6a0cf0
MiryangJung <miryang.dev@gmail.com>
2023-12-11 14:53:27 +0900

    home 페이지 스타일 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import PageLayout from "@/components/page-layout";
 import HomeBanner from "./components/banner";
 import RecentPosts from "./components/recent-posts";
 
 export default async function HomePage() {
   return (
-    <>
+    <PageLayout>
       <HomeBanner />
       <RecentPosts />
-    </>
+    </PageLayout>
   );
 }
