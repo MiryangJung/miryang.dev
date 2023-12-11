eb6dce8677147aee43d062c7f5ead47b991022a3
MiryangJung <miryang.dev@gmail.com>
2023-12-11 14:54:16 +0900

    resume 스타일 수정

--- app/resume/layout.tsx
+++ app/resume/layout.tsx
@@ -1,5 +1,5 @@
 import PageLayout from "@/components/page-layout";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <PageLayout className="px-0 py-10 bg-gray-50 dark:bg-gray-900">{children}</PageLayout>;
+  return <PageLayout className="p-5 bg-gray-50 dark:bg-gray-900">{children}</PageLayout>;
 }
