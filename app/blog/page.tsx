68531501580735dc07ebad852baf502a4d2cc678
MiryangJung <miryang.dev@gmail.com>
2024-03-18 21:36:03 +0900

    말줄임처리

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -18,8 +18,10 @@ export default function BlogPage() {
             className="flex py-5 items-start justify-between gap-2"
           >
             <div className="flex flex-col gap-1 flex-1">
-              <span className="font-bold text-lg break-keep">{blog.title}</span>
-              <span>{blog.description}</span>
+              <span className="font-bold text-lg break-all line-clamp-2">
+                {blog.title}
+              </span>
+              <span className="break-all">{blog.description}</span>
               <time className="text-xs text-gray-500 mt-1">{blog.date}</time>
             </div>
             <Image
