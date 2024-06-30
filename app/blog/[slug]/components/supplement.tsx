cd241ba4dad37c146d372dd60f4d69fc3bef61ae
MiryangJung <miryang.dev@gmail.com>
2024-07-01 02:54:36 +0900

    post 오픈소스 MDN Web Docs에 기여하는 방법

--- app/blog/[slug]/components/supplement.tsx
+++ app/blog/[slug]/components/supplement.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Supplement() {
   return (
-    <div className="p-2 border-l-4 border-yellow-600">
+    <div className="p-2 border-l-4 border-[#f5df4d]">
       <Link
         href="/guestbook"
-        className="font-bold underline underline-offset-4 text-yellow-600"
+        className="font-bold underline underline-offset-4 text-[#f5df4d]"
       >
         ✨ 방명록에 한 줄 남기고 가기 ✨
       </Link>
