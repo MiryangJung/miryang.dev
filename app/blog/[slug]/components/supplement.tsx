6d3cded16a60d9d1ffb13889d21c63c7cea66018
MiryangJung <miryang.dev@gmail.com>
2024-07-31 23:25:41 +0900

    색상 변경

--- app/blog/[slug]/components/supplement.tsx
+++ app/blog/[slug]/components/supplement.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Supplement() {
   return (
-    <div className="p-2 border-l-4 border-[#f5df4d]">
+    <div className="p-2 border-l-4 border-[#AFF4C6]">
       <Link
         href="/guestbook"
-        className="font-bold underline underline-offset-4 text-[#f5df4d]"
+        className="font-bold underline underline-offset-4 text-[#AFF4C6]"
       >
-        ✨ 방명록에 한 줄 남기고 가기 ✨
+        🍵 방명록에 한 줄 남기고 가기 🍵
       </Link>
     </div>
   );
